92ac2409006a60f46c844d36ed1105422606cecc
Chris Johnson <christopherjohnson1@gmail.com>
2020-03-07 12:55:20 -0600

    buttons work

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,14 @@ import navbar from './components/navbar/navbar';
 import '../styles/main.scss';
 import studentList from './components/studentList/studentList';
 import studentData from './helpers/data/studentData';
+import houseButtonGroup from './components/houseButtonGroup/houseButtonGroup';
+
 
 const init = () => {
   const allStudents = studentData.getStudents();
   navbar.loadNavbar();
   studentList.createStudentList(allStudents);
+  houseButtonGroup.createHouseButtonGroup();
 };
 
 init();
