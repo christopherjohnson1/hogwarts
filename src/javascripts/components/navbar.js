ca52a193823e3758b6b606d39cfd1b21faa57b42
Chris Johnson <christopherjohnson1@gmail.com>
2020-03-07 10:51:16 -0600

    names printing

--- src/javascripts/components/navbar.js
+++ src/javascripts/components/navbar.js
@@ -1,5 +1,4 @@
 import utils from '../helpers/utils';
-
 import './navbar/navbar.scss';
 
 const loadNavbar = () => {
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,12 @@
 import navbar from './components/navbar/navbar';
 import '../styles/main.scss';
+import studentList from './components/studentList/studentList';
+import studentData from './helpers/data/studentData';
 
-
-console.error('hi'); const init = () => {
+const init = () => {
+  const allStudents = studentData.getStudents();
   navbar.loadNavbar();
+  studentList.createStudentList(allStudents);
 };
 
 init();
